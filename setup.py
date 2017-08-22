dc6390bb26e6c558fc65de6c0f4245b606b90dd9
lfzyx <lfzyx.me@gmail.com>
2017-08-22 23:23:37 +0800

    Packaging and Distributing Projects https://packaging.python.org/tutorials/distributing-packages/

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, find_packages
 
-setup(name='buttersalt_ldap',
-      version='1.0',
-      author='lfzyx',
-      author_email='lfzyx.me@gmail.com',
-      url='https://github.com/lfzyx/ButterSalt-LDAP',
-      include_package_data=True,
-      packages=find_packages()
+setup(
+    name='buttersalt_ldap',
+    description='LDAP management plugin',
+    version='1.0.0',
+    author='lfzyx',
+    author_email='lfzyx.me@gmail.com',
+    url='https://github.com/lfzyx/ButterSalt-LDAP',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3 :: Only',
+        ],
+    python_requires='>=3.5.3',
+    keywords='buttersalt ldap',
+    packages=find_packages(),
+    package_data={
+        'buttersalt_ldap': ['templates/*/*.html'],
+    },
 )
