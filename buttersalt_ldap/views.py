54ce7c95f6915ba2dc2e7ed6a4c8723b0cac34e1
lfzyx <lfzyx.me@gmail.com>
2017-08-23 19:48:18 +0800

    plugin standardization

--- buttersalt_ldap/views.py
+++ buttersalt_ldap/views.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from ButterSalt import salt
 
 ldap = Blueprint('ldap', __name__, url_prefix='/ldap', template_folder='templates')
+__blueprint__ = ldap
 
 
 @ldap.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
+
     return render_template('ldap/index.html')
